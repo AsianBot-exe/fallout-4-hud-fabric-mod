2fdbe4e366fa54e5d74ef45f905da0a9e5aeba31
Ansel Dong <146680669+AsianBot-exe@users.noreply.github.com>
2026-06-25 20:20:27 -0700

    Initial commit

---
+++ src/client/java/fallout4hud/modid/client/Fallout4HUDClient.java
@@ -0,0 +1,10 @@
+package fallout4hud.modid.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class Fallout4HUDClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
